1da9ca5006a9281e58937a87b38800f793c7a0d4
yannidd <yanislavdonchev@gmail.com>
2021-04-11 21:01:08 +0100

    add serial plotter example

--- examples/tutorials/07-Serial-Plotter/07-Serial-Plotter.ino
+++ examples/tutorials/07-Serial-Plotter/07-Serial-Plotter.ino
@@ -1,6 +1,20 @@
+const int potentiometerPin = 6;  // Define the potentiometer pin.
+const int lightSensorPin = 6;    // Define the light sensor pin.
+
 void setup() {
-  Serial.begin(9600);               // Begin the Serial communication.
-  Serial.println("Hello, World!");  // Send a message to the computer.
+  pinMode(potentiometerPin, INPUT);  // Initialize potentiometerPin as an input.
+  pinMode(lightSensorPin, INPUT);    // Initialize lightSensorPin as an input.
+  Serial.begin(9600);                // Begin the Serial communication.
 }
 
-void loop() {}
+void loop() {
+  // Read the analogue values in the range [0, 1023].
+  int potentiometerValue = analogRead(potentiometerPin);
+  int lightSensorValue = analogRead(lightSensorPin);
+  // Send the readings to the computer in a space-separated format.
+  Serial.print(potentiometerValue);
+  Serial.print(" ");
+  Serial.println(-potentiometerValue);
+  // Wait for 100 milliseconds.
+  delay(100);
+}
