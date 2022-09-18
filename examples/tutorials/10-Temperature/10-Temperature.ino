150648fc712b26a80d7bb0973cf13fb29eee213f
Kasper Buckbee <kasper.buckbee@gmail.com>
2022-09-18 13:00:12 +0100

    Improved comments

--- examples/tutorials/10-Temperature/10-Temperature.ino
+++ examples/tutorials/10-Temperature/10-Temperature.ino
@@ -1,5 +1,6 @@
 #include <UkesfSixthFormers.h>
 
+//Temperature & Humidity Sensor
 //Change the value in the bracket below to:
 // --> DHT11 (if you have a blue sensor with a "D3" label on the Grove PCB)
 // --> DHT20 (if you have a black sensor with an "IIC" label on the Grove PCB)
