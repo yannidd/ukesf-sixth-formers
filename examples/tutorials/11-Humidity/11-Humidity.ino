f8213d387d22b82e6d75469846ed607174b9ca1e
Kasper Buckbee <kasper.buckbee@gmail.com>
2022-09-18 12:59:49 +0100

    Changed default sensor to DHT20

--- examples/tutorials/11-Humidity/11-Humidity.ino
+++ examples/tutorials/11-Humidity/11-Humidity.ino
@@ -1,9 +1,10 @@
 #include <UkesfSixthFormers.h>
 
+//Temperature & Humidity Sensor
 //Change the value in the bracket below to:
 // --> DHT11 (if you have a blue sensor with a "D3" label on the Grove PCB)
 // --> DHT20 (if you have a black sensor with an "IIC" label on the Grove PCB)
-DHTsensor tempHumiditySensor(DHT11);
+DHTsensor tempHumiditySensor(DHT20);
 
 void setup() {
   Serial.begin(9600);
