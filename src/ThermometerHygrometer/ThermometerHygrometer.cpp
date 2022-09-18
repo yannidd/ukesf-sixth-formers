e6cd943876db7a983051f248e58f5f304ad503ea
Kasper Buckbee <kasper.buckbee@gmail.com>
2022-09-18 12:14:06 +0100

    Single sensor interface for DHT11 and DHT20

--- src/ThermometerHygrometer/ThermometerHygrometer.cpp
+++ src/ThermometerHygrometer/ThermometerHygrometer.cpp
@@ -1,24 +1,47 @@
 #include "ThermometerHygrometer.h"
 
 #include "DHT.h"
+#include <Adafruit_AHTX0.h>
 
+static Adafruit_AHTX0 AHT20_sensor;
+static DHT DHT11_sensor(3, DHT11);
 static bool hasBegun = false;
-static DHT dht(3, DHT11);
 
-int Thermometer::begin() {
+DHTsensor::DHTsensor(uint8_t type) {
+  _type = type;
+}
+
+int DHTsensor::begin() {
   if (hasBegun) return true;
-  hasBegun = true;
-  dht.begin();
+  if (_type == DHT20){
+    hasBegun = true;
+	AHT20_sensor.begin();
+  }
+  else if (_type == DHT11){
+    hasBegun = true;
+	DHT11_sensor.begin();
+  }
   return hasBegun;
 }
 
-float Thermometer::read() { return dht.readTemperature(); }
-
-int Hygrometer::begin() {
-  if (hasBegun) return true;
-  hasBegun = true;
-  dht.begin();
-  return hasBegun;
+float DHTsensor::readTemperature() {
+  if (_type == DHT20){
+    sensors_event_t humiditySensor, tempSensor;
+    AHT20_sensor.getEvent(&humiditySensor, &tempSensor);
+    return tempSensor.temperature;
+  }
+  else if (_type == DHT11){
+    return DHT11_sensor.readTemperature();
+  }
 }
 
-float Hygrometer::read() { return dht.readHumidity(); }
+float DHTsensor::readHumidity(){
+  if (_type == DHT20){
+    sensors_event_t humiditySensor, tempSensor;
+    AHT20_sensor.getEvent(&humiditySensor, &tempSensor);
+    return humiditySensor.relative_humidity; 
+  }
+  else if (_type == DHT11){
+    return DHT11_sensor.readTemperature();
+  }
+}
